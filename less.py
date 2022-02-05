c36eb212dce8ab653ae3f67d33b8996a81215191
VaheArakel <vahearakelyan39@gmail.com>
2022-02-05 14:27:44 +0400

    new file

---
+++ less.py
@@ -0,0 +1,424 @@
+# # import random
+# # while True:
+# #     pc = random.radiant(1, 20)
+# #     if pc > 10:
+# #         print(pc)
+# #     break
+    
+
+# import random
+# komp = 'Rock', 'Papper', 'Scisser'
+# pc = random.choice(komp)
+# user = input('Enter.....')
+# user_point = 0
+# pc_point = 0
+#     if pc == 'Rock' and user == 'Papper' or pc == 'Papper' and user == 'Scisser' or pc == 'Scisser' and user == 'Rock'
+#     user_point += 1
+#     print('Win user! user point', user_point, 'Lose pc pc point', pc_point)
+#     pc1 = random.choice(komp)
+#     user1 = input('Enter.....')
+#         if pc1 == 'Rock' and user1 == 'Papper' or pc1 == 'Papper' and user1 == 'Scisser' or pc1 == 'Scisser' and user1 == 'Rock'
+#             user_point += 1
+#             print( 'END GAME!!! \nUser Win user point', user_point, 'Lose pc pc point', pc_point)
+#         elif user1 == 'Rock' and pc1 == 'Papper' or user1 == 'Papper' and pc1 == 'Scisser' or user1 == 'Scisser' and pc1 == 'Rock'
+#             pc_point += 1
+#             print('Win pc pc point', pc_point, 'Lose user user point', user_point)
+#             pc2 = random.choice(komp)
+#             user2 = input('Enter.....')
+#             if pc2 == 'Rock' and user2 == 'Rock' or pc2 == 'Papper' and user2 == 'Papper' or pc2 == 'Scisser' and user2 == 'Scisser'
+#                 print(EQUAL!!!)
+#             elif pc2 == 'Rock' and user2 == 'Papper' or pc2 == 'Papper' and user2 == 'Scisser' or pc2 == 'Scisser' and user2 == 'Rock'
+#                 user_point += 1
+#                 print('END GAME!!! \nUser Win user point', user_point, 'Lose pc pc point', pc_point)
+#             elif user2 == 'Rock' and pc2 == 'Papper' or user2 == 'Papper' and pc2 == 'Scisser' or user2 == 'Scisser' and pc2 == 'Rock'
+#                 pc += 1
+#                 print('END GAME!!! \nWin pc pc point', pc_point, 'Lose user user point', user_point)
+#     elif user == 'Rock' and pc == 'Papper' or user == 'Papper' and pc == 'Scisser' or user == 'Scisser' and pc == 'Rock'
+#         pc_point += 1
+#         print('Win ps pc point', pc_point, 'Lose user user point', user_point)
+#         pc3 = random.choice(komp)
+#         user3 = input(Enter.....)
+#         if user3 == 'Rock' and pc3 == 'Papper' or user3 == 'Papper' and pc3 == 'Scisser' or user3 == 'Scisser' and pc3 == 'Rock'
+#             pc_point += 1
+#             print('END GAME!!! \nWin pc pc point', pc_point, 'Lose user user point', user_point)
+#         elif pc3 == 'Rock' and user3 == 'Papper' or pc3 == 'Papper' and user3 == 'Scisser' or pc3 == 'Scisser' and user3 == 'Rock'
+#             user_point += 1
+#             print('Win user! user point', user_point, 'Lose pc pc point', pc_point)
+#             pc4 = random.choice(komp)
+#             user4 = input(Enter.....)
+#             if pc4 == 'Rock' and user4 == 'Rock' or pc4 == 'Papper' and user4 == 'Papper' or pc4 == 'Scisser' and user4 == 'Scisser'
+#                 print('EQUAL!!!')
+#             elif pc4 == 'Rock' and user4 == 'Papper' or pc4 == 'Papper' and user4 == 'Scisser' or pc4 == 'Scisser' and user4 == 'Rock'
+#                 user_point += 1
+#                 print('END GAME!!! \nUser Win user point', user_point, 'Lose pc pc point', pc_point')
+#             elif user4 == 'Rock' and pc4 == 'Papper' or user4 == 'Papper' and pc4 == 'Scisser' or user4 == 'Scisser' and pc4 == 'Rock'
+#                 pc_point += 1
+#                 print('END GAME!!! \nWin pc pc point', pc_point, 'Lose user user point', user_point)
+#     elif pc == 'Rock' and user == 'Rock' or pc == 'Papper' and user == 'Papper' or pc == 'Scisser' and user == 'Scisser'
+#         print('EQUAL')
+
+# number = int(input('Enter number'))
+# res = 1 
+# for i in range (number):
+#     print(res)
+
+# #Parz_tver
+# n = int(input('Enter number...'))
+# for i in range(1, n+1):
+#     if i > 1:
+#         for j in range(2, i):
+#             if (i % j) == 0:
+#                 break
+#         else:
+#             print(i)
+
+#amenapoqr @ndhanur bazmapatik
+
+# n = int(input('Enter number1...'))
+# m = int(input('Enter number2...'))
+# for i in range(n, (n * m) + 1):
+#     if i % n == 0 and i % m == 0:
+#         print(i)
+#         break
+# zuyger = 0
+# kenter = 0
+# for i in range (1, 101):
+#     if i % 2 == 0:
+#         zuyger += 1
+#     else:
+#         kenter += 1
+# print(zuyger, kenter)
+
+# text = input('Enter your text')
+# gumar = 0
+# for i in text:
+#     if i.isdigit():
+#         gumar += int(i)
+# print(gumar)
+ 
+# number = int(input('Enter your number'))
+# gumar = 0
+# for i in str(number):
+#     gumar += int(i)
+# print(gumar)
+
+# while True:
+#     age = int(Inpout('Enter age range (18-20)---'))
+#     ser = input ('M or F---')
+#     if 18 <= age <= 20
+
+# for i in 'python':
+#     if i == 'y':
+#         break
+#     print (i)
+# for i in 'python':
+#     if i == 't':
+#         break
+#     print (i, end =' ')
+# print(' ')
+# for i in 'python':
+#     if i == 'h':
+#         break
+#     print (i, end= ' ')
+# print('')
+# for i in 'python':
+#     if i == 'o':
+#         break
+#     print (i, end =' ')    
+# print('')
+# for i in 'python':
+#     if i == 'n':
+#         break
+#     print (i, end =' ')
+# print('')
+# for i in 'python ':
+#     if i == ' ':
+#         break
+#     print (i, end =' ')
+# print('')
+# for i in 'python':
+#     if i == 'n':
+#         break
+#     print (i, end =' ')
+# print('')
+# for i in 'python':
+#     if i == 'o':
+#         break
+#     print (i, end =' ')
+# print(' ')
+# for i in 'python':
+#     if i == 'h':
+#         break
+#     print (i, end= ' ')
+# print('')
+# for i in 'python':
+#     if i == 't':
+#         break
+#     print (i, end =' ')
+# print('')
+# for i in 'python':
+#     if i == 'y':
+#         break
+#     print(i)
+
+#Factorial
+
+# number = int(input('Enter your number...'))
+# res = 1
+# for i in range(number):
+#     res = (res * (i+1))
+#     print(res)
+
+# number = int(input('Enter your number...'))
+# res = 0
+# for i in range(1, number + 1):
+#     res = (res + i)
+#     print(res/i)
+
+# number = int(input('Enter your number...'))
+# b = int(input('Enter your first value...'))
+# q = int(input('Enter your constant...'))
+# print(b)
+# res = (b*q)
+# for i in range(1, number):
+#     res = (res*b)
+#     print(res)
+
+# number = int(input('Enter your number...'))
+# res = 1
+# for i in range(1, 11):
+#     res = number * i
+#     if i == 2:
+#         break
+#     print (number, '*', i, '=', res)
+# for i in range(2, 11):
+#     res = number * i
+#     if i == 3:
+#         break
+#     print (number, '*', i, '=', res)
+# for i in range(3, 11):
+#     res = number * i
+#     if i == 4:
+#         break
+#     print (number, '*', i, '=', res)
+# for i in range(4, 11):
+#     res = number * i
+#     if i == 5:
+#         break
+#     print (number, '*', i, '=', res)
+# for i in range(5, 11):
+#     res = number * i
+#     if i == 6:
+#         break
+#     print (number, '*', i, '=', res)
+# for i in range(6, 11):
+#     res = number * i
+#     if i == 7:
+#         break
+#     print (number, '*', i, '=', res)
+# for i in range(7, 11):
+#     res = number * i
+#     if i == 8:
+#         break
+#     print (number, '*', i, '=', res)
+# for i in range(8, 11):
+#     res = number * i
+#     if i == 9:
+#         break
+#     print (number, '*', i, '=', res)
+# for i in range(9, 11):
+#     res = number * i
+#     if i == 10:
+#         break
+#     print (number, '*', i, '=', res)
+# for i in range(10, 11):
+#     res = number * i
+#     if i == 11:
+#         break
+# #     print (number, '*', i, '=', res)
+
+#TARETIV
+# taretiv = int(input('enter your birth year...'))
+# if taretiv % 12 == 0:
+#     print('your zodiac sign is Monkey')
+# elif taretiv % 12 == 1:
+#     print('your zodiac sign is Rooster')
+# elif taretiv % 12 == 2:
+#     print('your zodiac sign is Dog')
+# elif taretiv % 12 == 3:
+#     print('your zodiac sign is Pig')
+# elif taretiv % 12 == 4:
+#     print('your zodiac sign is Rat')
+# elif taretiv % 12 == 5:
+#     print('your zodiac sign is Ox')
+# elif taretiv % 12 == 6:
+#     print('your zodiac sign is Tiger')
+# elif taretiv % 12 == 7:
+#     print('your zodiac sign is Rabbit')
+# elif taretiv % 12 == 8:
+#     print('your zodiac sign is Dragon')
+# elif taretiv % 12 == 9:
+#     print('your zodiac sign is Snake')
+# elif taretiv % 12 == 10:
+#     print('your zodiac sign is Horse')
+# elif taretiv % 12 == 11:
+#     print('your zodiac sign is Goat')
+
+
+# number = 0
+# sq = 1
+# while number < 10:
+#     number += 1
+#     sq = number * number
+#     print(number , sq)
+
+# number = int(input('Enter your number...'))
+# res = 0
+# for i in range(1, number + 1):
+#     number = number + i
+#     res = number * number
+#     print(res)
+
+# list1 = [1, 5, 10]
+# res = 1
+# for i in list1:
+#     res *= i
+# print (res)
+
+# list2 = ['Vahe', 'Lilit', 'Gohar', 'Daniel']
+# list3 = []
+# for i in list2:
+#     if len(i) not in list3:
+#         list3.append(i)
+
+# list1 = ['Vahe', 'Lilit', 'Gohar', 'Daniel', 'Lilit', 'Vahe']
+# list2 = []
+# for i in list1:
+#     if i not in list2:
+#         list2.append(i)
+# print(list2)
+
+
+# list1 = ['Vahe', 'Lilit', 'Gohar', 'Daniel', 'Lilit', 'Vahe']
+# list2 = []
+# for i in list1:
+#     if i not in list2:
+#         list2.append(i)
+# print(list1)
+
+
+
+
+# my_list = ['Hello world', 'Barev', 'Hi']
+# count = 0
+# for i in my_list:
+#     if len(i) > count:
+#         count = len(i)
+#         res = i
+# print(res)
+
+# list1 = ['Anun1', 'Anun2', 'Anun3']
+# list2 = ['Anun11', 'Anun2', 'Anun13']
+# for i in list1:
+#     if i in list2:
+#         print('True')
+#         break
+# else:
+#     print('False')
+
+# link = 'shdhgkwgfyufgyuewfjhwebkjewhg=gffjygyu'
+# print(link[(link.index('=') + 1) :])
+
+# list = ['Poxos', 'Petros', 'Martiros', 'Nikoxos', 'Armen']
+# for i in range(len(list)):
+#     print(list[i], "---", list[(i + 1) % (len(list))])
+
+# mydict = {'name1': 'Vahe', 'name2': 'Daniel'}
+# for i in mydict.values():
+#     print(i)
+
+# mydict = {'name': 'Mustang', 'year': 1994}
+# mydict['year'] = 2014
+# print(mydict)
+
+# mydict = {'Vahe': 42, 'Daniel': 21, 'Gohar': 27}
+# for i in mydict.values():
+#     if i < i + 1:
+    
+#     print(i)
+
+# mydict = {'Vahe': 42, 'Daniel': 21, 'Gohar': 27}
+# mydict ['Artak'] = 33
+# print(mydict)
+
+# mydict = {'Vahe': 42, 'Daniel': 21, 'Gohar': 27, 'Artak': 33}
+# for i in mydict.values():
+#     if i == 33:
+#         print('yes')
+#     else:
+#         break
+
+# mydict1 = {'name1': 22, 'name2': 33}
+# mydict2 = {'name3': 44, 'name4': 55}
+# mydict1.update(mydict2)
+# print(mydict1)
+
+# multiply = 1
+# mydict = {'name1': 22, 'name2': 33, 'name3': 44}
+# for i in mydict.values():
+#     multiply *= i
+# print(multiply)
+
+# mydict = {'D': 56, 'E': 12, 'F': 69, 'C': 45, 'B': 23, 'A': 67}
+# list_values = []
+# for i in mydict.values():
+#     list_values.append(i)
+#     x = sorted(list_values)
+# print(x)
+
+# s = 'a,2,b,5,c,8,d,4,e,11'
+# list_tver = []
+# list_tarer = []
+# mydict = {}
+# for i in s:
+#     if i.isdigit():
+#         list_tver.append(i)
+#     elif i.isalpha() and i != ',':
+#         list_tarer.append(i)
+# for x, y in zip(list_tarer, list_tver):
+#     mydict.setdefault(x, y)
+# print(mydict)
+
+# set1 = {'apple', 'banana', 'orange', 'cherry'}
+# for i in (set1):
+#     if i == 'banana':
+#         continue
+#     print(i)
+
+# set1 = {'apple', 'banana', 'orange', 'cherry'}
+# set1.add('limon')
+# print(set1)
+
+# set1 = {'apple', 'banana', 'orange', 'cherry'}
+# set1.discard('banana')
+# print(set1)
+
+# set1 = {'apple', 'banana', 'orange', 'cherry'}
+# set2 = {'banana', 'orange'}
+# print(set1.intersection(set2))
+
+# set1 = {'apple', 'banana', 'orange', 'cherry'}
+# set2 = {'melon', 'watermelon', 'limon'}
+# print(set1.union(set2))
+
+# set1 = {'apple', 'banana', 'orange', 'cherry'}
+# set1.clear()
+# print(set1)
+
+# set2 = {2, 3, 43, 55, 38, 86, 22, 63, 91, 17}
+# print(max(set2))
+# print(min(set2))
+
+# set1 = {'apple', 'banana', 'orange', 'cherry'}
+# print(len(set1))
+
